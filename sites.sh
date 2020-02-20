82a0e8f51bc26b8b1a3f1e7efeb2e4d88f2a0aa7
Gaëtan PRIOUR <gpriour@previsite.com>
2020-02-20 17:06:58 +0100

    Add Nginx and refactor

--- sites.sh
+++ sites.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-HOSTS='/etc/apache2/sites-enabled/'
+APACHE_VHOSTS=/etc/apache2/sites-enabled
+NGINX_VHOSTS=/etc/nginx/sites-enabled
 
-for file in `ls $HOSTS`
-do
-        echo $file
-        cat $HOSTS$file | grep --color -E "ServerName|DocumentRoot"
-done
+function show_vhosts {
+    for file in `ls $1`
+    do
+        echo -e "\n$1/$file"
+        cat $1/$file | grep --color -E "ServerName|DocumentRoot|server_name|root"
+    done
+}
+
+if [[ -d $APACHE_VHOSTS ]]; then
+    show_vhosts $APACHE_VHOSTS
+fi
+
+if [[ -d $NGINX_VHOSTS ]]; then
+    show_vhosts $NGINX_VHOSTS
+fi
+
